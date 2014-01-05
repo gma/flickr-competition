991d8236235e1f99d57bedc8b77a4d947afaf41e
Graham Ashton <graham@effectif.com>
2014-01-05 20:10:25 +0000

    First working version
    Used on the December 2013 Micro 4/3 UK Flickr group competition.

---
+++ tally.py
@@ -0,0 +1,103 @@
+import htmllib
+import os
+import re
+import sys
+
+import lxml.html
+import requests
+
+
+class Pattern:
+
+    @classmethod
+    def prefix(cls):
+        return r'(?:Number|No.|#)\s*'
+
+    @classmethod
+    def with_prefix_or_suffix(cls, regex, prefix, suffix):
+        return r'(?:(?:%s)%s|%s(?:%s))' % (prefix, regex, regex, suffix)
+
+    @classmethod
+    def entry(cls, capture = False):
+        entry = r'(\d+)' if capture else r'\d+'
+        prefix = r'(?:Number|No.|#)\s*'
+        suffix = r'#'
+        return cls.with_prefix_or_suffix(entry, prefix, suffix)
+
+    @classmethod
+    def suffix(cls):
+        return r'\s*p(?:oin)?ts?'
+
+    @classmethod
+    def delimiter(cls):
+        return r'[\s:=-]+'
+
+    @classmethod
+    def vote_splitter(cls):
+        pattern = cls.entry() + cls.delimiter() + r'\d+' + cls.suffix()
+        return re.compile(pattern, re.MULTILINE)
+
+    @classmethod
+    def score_splitter(cls):
+        pattern = cls.entry(True) + cls.delimiter() + r'(\d+)' + cls.suffix()
+        return re.compile(pattern)
+
+
+class Vote:
+
+    @staticmethod
+    def tally(votes):
+        result = {}
+        for vote in votes:
+            for entry, points in vote.points().items():
+                result[entry] = result.setdefault(entry, 0) + points
+        return result
+
+    def __init__(self, text):
+        self.text = text
+
+    def points(self):
+        points = {}
+        for score in re.findall(Pattern.vote_splitter(), self.text):
+            match = re.match(Pattern.score_splitter(), score)
+            if match:
+                entry, score = [num for num in match.groups() if num is not None]
+                points[entry] = int(score)
+        if len(points) != 3:
+            message = "%d votes found in '%s'\n" % (len(points), self.text)
+            sys.stderr.write(message)
+            return {}
+        else:
+            return points
+
+
+class App:
+
+    def usage(self):
+        script = os.path.basename(sys.argv[0])
+        sys.stderr.write("Usage: %s <discussion-html>\n" % script)
+        sys.exit(1)
+
+    def main(self, url):
+        response = requests.get(url)
+        if response.ok:
+            html = lxml.html.fromstring(response.text)
+            messages = html.cssselect('table.TopicReply tr p')
+            votes = [Vote(message.text_content()) for message in messages]
+            print "RESULTS"
+            scores = Vote.tally(votes)
+            results = sorted(scores, key = scores.get)
+            results.reverse()
+            for entry in results:
+                print "#%s - %d points" % (entry, scores[entry])
+        else:
+            sys.stderr.write(response.text + "\n")
+
+
+if __name__ == '__main__':
+    app = App()
+    try:
+        url = sys.argv[1]
+        App().main(url)
+    except LookupError:
+        app.usage()
---
+++ vote_test.py
@@ -0,0 +1,36 @@
+import unittest
+import tally
+
+
+class VoteTest(unittest.TestCase):
+
+    def expected(self):
+        return { '14': 1, '8': 2, '6': 3 }
+
+    def test_points_returns_dict_of_scores(self):
+        vote = tally.Vote("#6 - 3 pts\n#8 - 2 pts\n#14 - 1 pt")
+        self.assertEqual(vote.points(), self.expected())
+
+    def test_points_isnt_sensitive_to_whitespace(self):
+        vote = tally.Vote("#6- 3 pts\n# 8 - 2pts\n #14 - 1 pt")
+        self.assertEqual(vote.points(), self.expected())
+
+    def test_points_works_with_variety_of_delimiters(self):
+        vote = tally.Vote("#6 3 pts. #8: 2pts\n #14 = 1 pt")
+        self.assertEqual(vote.points(), self.expected())
+
+    def test_points_is_relaxed_about_prefix_and_suffix(self):
+        vote = tally.Vote("No 6 - 3 pts\nNo. 8 - 2 points\n14# - 1 point")
+        self.assertEqual(vote.points(), self.expected())
+
+    def test_tally_adds_up_all_the_scores(self):
+        votes = [
+            tally.Vote("#6 - 3 pts\n#8 - 2 pts\n#14 - 1 pt"),
+            tally.Vote("#5 - 3 pts\n#6 - 2 pts\n#8 - 1 pt")
+        ]
+        results = { '6': 5, '5': 3, '8': 3, '14': 1 }
+        self.assertEqual(tally.Vote.tally(votes), results)
+
+
+if __name__ == '__main__':
+    unittest.main()
