c3cd0bbe0b0c63372e659efa3d26d8c882f78a9e
Graham Ashton <graham@effectif.com>
2014-01-07 09:56:09 +0000

    Rename Pattern to MessagePattern

--- tally.py
+++ tally.py
@@ -6,7 +6,7 @@ import lxml.html
 import requests
 
 
-class Pattern:
+class MessagePattern:
 
     @classmethod
     def prefix(cls):
@@ -57,8 +57,8 @@ class Vote:
 
     def points(self):
         points = {}
-        for score in re.findall(Pattern.vote_splitter(), self.text):
-            match = re.match(Pattern.score_splitter(), score)
+        for score in re.findall(MessagePattern.vote_splitter(), self.text):
+            match = re.match(MessagePattern.score_splitter(), score)
             if match:
                 entry, score = [num for num in match.groups() if num is not None]
                 points[entry] = int(score)
