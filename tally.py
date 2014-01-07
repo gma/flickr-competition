9988d8e5afd05cea225713088606d26db4d1ba40
Graham Ashton <graham@effectif.com>
2014-01-07 09:43:49 +0000

    Be more careful when catching LookupError

--- tally.py
+++ tally.py
@@ -96,6 +96,7 @@ if __name__ == '__main__':
     app = App()
     try:
         url = sys.argv[1]
-        App().main(url)
     except LookupError:
         app.usage()
+    else:
+        app.main(url)
